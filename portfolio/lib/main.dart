0c0e24957f838a3e93dfc86b1e6cf164d2114bc7
HebaBakry <Heba.m.bakry@gmail.com>
2025-05-28 15:34:16 +0200

    create flutter project

---
+++ portfolio/lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portfolio',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: const TextTheme(
+          displayMedium: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w700,
+            color: Color(0xFF21243D),
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: Color(0xFF21243D),
+            letterSpacing: 0,
+          ),
+          displaySmall: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFFFF6464),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(2),
+            ),
+            fixedSize: const Size(208, 47),
+            padding: EdgeInsets.zero,
+          ),
+        ),
+      ),
+      home: const PortfolioPage(),
+    );
+  }
+}
+
+class PortfolioPage extends StatelessWidget {
+  const PortfolioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 86),
+          CircleAvatar(
+            radius: 80,
+            backgroundImage: AssetImage('assets/images/person.jpg'),
+          ),
+          SizedBox(height: 34),
+          Text(
+            'Hi, i am Heba,\nCreative\n Technologist',
+            style: Theme.of(context).textTheme.displayMedium,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 24),
+          SizedBox(
+            width: 330,
+            child: Text(
+              'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+              style: Theme.of(context).textTheme.bodyMedium,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: () {},
+            style: Theme.of(context).elevatedButtonTheme.style,
+            child: Text(
+              'Download Resume',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
